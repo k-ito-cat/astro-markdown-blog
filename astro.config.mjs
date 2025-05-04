cb026185aa2b7fee0e1b19c5bebd394bb477f271
k-ito-cat <kyohy163@gmail.com>
2025-05-04 11:11:58 +0900

    chore: アダプタの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import decapCmsOauth from "astro-decap-cms-oauth";
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
-
+  experimental: {
+    session: true,
+  },
   integrations: [decapCmsOauth()],
 });
