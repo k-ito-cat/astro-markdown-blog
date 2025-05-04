172bd2670285e7b5dc479df1c74d1563dd3009a7
k-ito-cat <kyohy163@gmail.com>
2025-05-04 22:36:04 +0900

    delete: 不要なので削除

--- gen-headers.js
+++
@@ -1,8 +0,0 @@
-import { mkdirSync, writeFileSync } from "fs";
-import "dotenv/config";
-
-const auth = process.env.BASIC_AUTH;
-
-const contents = ["/admin/*", `  Basic-Auth: ${auth}`].join("\n") + "\n";
-mkdirSync("public", { recursive: true });
-writeFileSync("public/_headers", contents);
