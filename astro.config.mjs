e5f9396ea6ae362daa7a88611d13f05838cb24fa
k-ito-cat <kyohy163@gmail.com>
2025-05-04 11:01:05 +0900

    chore: 微調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import decapCmsOauth from "astro-decap-cms-oauth";
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
