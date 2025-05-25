8a8c264a3bd175501768e5d827cb39a42f070d8e
k-ito-cat <kyohy163@gmail.com>
2025-05-25 18:01:49 +0900

    style: calloutスタイル追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import netlify from "@astrojs/netlify";
 import icon from "astro-icon";
+import remarkGithubBlockquoteAlert from "remark-github-blockquote-alert";
+import rehypeRaw from "rehype-raw";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +12,13 @@ export default defineConfig({
   adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    remarkPlugins: [remarkGithubBlockquoteAlert],
+    rehypePlugins: [rehypeRaw],
+    remarkRehype: {
+      allowDangerousHtml: true,
+    },
   },
   experimental: {
     session: true,
