1dcf8b3218fd6b5c4c19457512f2c6b87e2b8991
k-ito-cat <91656115+k-ito-cat@users.noreply.github.com>
2026-09-06 14:54:17 +0900

    style: コードコピー操作の表示を統一する

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,15 @@ import createSuggester from "./src/plugins/createSuggester.mjs";
 import tagEditor from "./src/plugins/tagEditor.mjs";
 import blockRange from "./src/plugins/blockRange.mjs";
 
-import astroExpressiveCode from "astro-expressive-code";
+import astroExpressiveCode, {
+  createInlineSvgUrl,
+  pluginFramesTexts,
+} from "astro-expressive-code";
+
+pluginFramesTexts.overrideTexts(undefined, {
+  copyButtonTooltip: "コードをコピー",
+  copyButtonCopied: "コピーしました",
+});
 
 // https://astro.build/config
 export default defineConfig({
@@ -77,6 +85,19 @@ export default defineConfig({
         codeBackground: "var(--color-code-bg)",
         borderRadius: "2px",
         borderColor: "var(--border)",
+        frames: {
+          copyIcon: createInlineSvgUrl([
+            `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'>`,
+            `<path d='M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z'/>`,
+            `<path d='M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z'/>`,
+            `</svg>`,
+          ]),
+          inlineButtonForeground: "var(--color-code-text)",
+          inlineButtonBackgroundIdleOpacity: "0",
+          inlineButtonBackgroundHoverOrFocusOpacity: "0",
+          inlineButtonBackgroundActiveOpacity: "0",
+          inlineButtonBorderOpacity: "0",
+        },
       },
     }),
     // expressive-code の後に置くこと。rehype プラグインを最後に走らせる必要がある
