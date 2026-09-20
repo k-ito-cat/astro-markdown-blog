d8e067e6ed0addfd6a1c952fa5cb243ad8be6fe2
k-ito-cat <91656115+k-ito-cat@users.noreply.github.com>
2026-09-21 01:15:03 +0900

    style(timeline): 日付グループから年表記を省く

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -8,4 +8,15 @@ const formatter = new Intl.DateTimeFormat("ja-JP", {
 export const toIsoDate = (date: Date) =>
   new Date(date).toISOString().split("T")[0];
 
-export const formatJapaneseDate = (date: Date) => formatter.format(new Date(date));
+/* 年は年の断層線が持つので、日付groupは月日だけを出す */
+const monthDayFormatter = new Intl.DateTimeFormat("ja-JP", {
+  month: "long",
+  day: "numeric",
+  timeZone: "UTC",
+});
+
+export const formatJapaneseDate = (date: Date) =>
+  formatter.format(new Date(date));
+
+export const formatJapaneseMonthDay = (date: Date) =>
+  monthDayFormatter.format(new Date(date));
